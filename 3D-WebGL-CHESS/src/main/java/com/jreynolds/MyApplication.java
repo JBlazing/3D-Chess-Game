766e37b638e69371a1bf8a5d05b7b6523a9bf72b
jblazing <john@johnpreynolds.com>
2025-03-14 19:23:35 -0400

    Commit base app and add .idea to gitignore

---
+++ 3D-WebGL-CHESS/src/main/java/com/jreynolds/MyApplication.java
@@ -0,0 +1,16 @@
+package com.jreynolds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication(scanBasePackages = "com.jreynolds")
+public class MyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+	}
+
+}
