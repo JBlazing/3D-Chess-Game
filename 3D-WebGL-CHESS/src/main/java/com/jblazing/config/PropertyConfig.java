79519c9b56cd60ad73ffd07932e22b77c570ea85
jblazing <john@johnpreynolds.com>
2025-03-29 16:34:06 -0400

    Fix prop file

--- 3D-WebGL-CHESS/src/main/java/com/jblazing/config/PropertyConfig.java
+++ 3D-WebGL-CHESS/src/main/java/com/jblazing/config/PropertyConfig.java
@@ -1,9 +1,7 @@
 package com.jblazing.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource(value = "file:///etc/jblazing/chess.properties", ignoreResourceNotFound = true)
